d67bac26296c61ef4b0402966301ae1841b7e095
ranog <jprnogueira@yahoo.com.br>
2017-01-04 22:46:08 -0200

    Aplicação web sem estados

--- placeholder/placeholder.py
+++ placeholder/placeholder.py
@@ -1,12 +1,14 @@
 import hashlib # retorna um valor opaco de ETag.
 import os
 import sys
-
+from io import BytesIO
+from PIL import Image, ImageDraw
 from django.conf import settings
 
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
 SECRET_KEY = os.environ.get('SECRET_KEY', '9+n(1i@3voejxdb1*y0#hp)nxju#zzr4nk5u-6v=r#utomfzpl')
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+BASE_DIR = os.path.dirname(__file__)
 
 settings.configure(
     DEBUG=DEBUG,
@@ -18,15 +20,36 @@ settings.configure(
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
     ),
+    
+    INSTALLED_APPS=(
+        'django.contrib.staticfiles',
+    ),
+
+    # no Django 1.10:
+    TEMPLATES = [{
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+    }],
+    
+    # no livro:
+    #TEMPLATE_DIRS = [ 
+    #    os.path.join(BASE_DIR, 'templates'),
+    #],
+
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
+    ],
+    
+    STATIC_URL = '/static/'
 )
 
-from io import BytesIO
-from PIL import Image, ImageDraw
 from django import forms
 from django.conf.urls import url
 from django.core.cache import cache # verifica se a imagem já está armazenada.
+from django.core.urlresolvers import reverse
 from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import render
 from django.views.decorators.http import etag
 
 class ImageForm(forms.Form):
@@ -49,7 +72,7 @@ class ImageForm(forms.Form):
             text = '{} X {}'.format(width, height)
             textwidth, textheight = draw.textsize(text)
             if textwidth < width and textheight < height:
-                texttop = (height - texttheight) // 2
+                texttop = (height - textheight) // 2
                 textleft = (width - textwidth) // 2
                 draw.text((textleft, texttop), text, fill=(255, 255, 255))
             content = BytesIO()
@@ -75,7 +98,11 @@ def placeholder(request, width, height):
         return HttpResponseBadRequest('Invalid Image Request')
 
 def index(request):
-    return HttpResponse('Hello Word')
+    example = reverse('placeholder', kwargs={'width': 50, 'height': 50})
+    context = {
+        'example' : request.build_absolute_uri(example)
+    }
+    return render(request, 'home.html', context)
 
 urlpatterns = [
     url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/?', placeholder,
